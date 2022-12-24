97ca590c2e141c06bd94472e3900722a2930f293
McCReuben <thingkr@gmail.com>
2022-12-23 22:28:36 -0500

    [dev] Preprosessing, initialize streamlit package

---
+++ kmeans.R
@@ -0,0 +1,10 @@
+# install.packages('coefplot')
+# install.packages("ClusterR")
+# install.packages("cluster")
+# install.packages("installr")
+
+library(coefplot)
+library(glmnet)
+library(ClusterR)
+library(cluster)
+
