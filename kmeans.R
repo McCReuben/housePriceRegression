7e9ad67289d5f58644c1dd17080f0620a0680386
McCReuben <thingkr@gmail.com>
2022-12-29 17:06:37 -0500

    [dev] Format, library

--- kmeans.R
+++ kmeans.R
@@ -1,10 +1,4 @@
-# install.packages('coefplot')
-# install.packages("ClusterR")
-# install.packages("cluster")
-# install.packages("installr")
-
 library(coefplot)
 library(glmnet)
 library(ClusterR)
 library(cluster)
-
