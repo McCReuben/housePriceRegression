1a5496e926ef4952f496fa9fd4a7b63829b55d9d
McCReuben <thingkr@gmail.com>
2023-01-05 17:58:43 -0500

    [dev] Modular get_data fundtion

---
+++ get_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def get_data():
+    final_test = pd.read_csv('data/test.csv')
+    train_init = pd.read_csv('data/train.csv')
+
+    labels = train_init["SalePrice"]
+    features = train_init.drop(columns=["SalePrice"])
+
+    # Set up training and testing data
+    x_train,x_test,y_train,y_test=train_test_split(features, labels, test_size=0.2, random_state=7)
+    return x_train,x_test,y_train,y_test
